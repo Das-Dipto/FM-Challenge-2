442b28b6b8e761a8f7adca21d411313afa3525f7
Dipto Das <ddipto396@gmail.com>
2022-12-11 23:35:21 -0800

    tip amount and total amount calculation logic added

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let bill = document.getElementById('bill');
 let people = document.getElementById('people');
 let totalAmount = document.querySelector('.total-bill');
 let tipAmount = document.querySelector('.tip-amount');
+let percentage = document.querySelectorAll('.percentage');
+let prc = document.querySelectorAll('.pr');
 
 let num = 0;
 let pep = 0;
@@ -9,6 +11,9 @@ let k = 1;
 let i = false;
 let num1 =1;
 let num2 =1;
+let temp = 0;
+let tip = 0;
+const arr = ["5%", "10%", "15%", "25%", "50%"];
 const billMuliplier = (a, z) =>{
        if ( i == z){
         num1 = a;
@@ -17,19 +22,52 @@ const billMuliplier = (a, z) =>{
         num2 = a;
        }
        totalAmount.innerHTML = `$ ${parseFloat(num1*num2).toFixed(2)}`;
+       temp = parseFloat(num1*num2).toFixed(2);
+
+       percentage.forEach((item, index)=>{
+        item.addEventListener("click", ()=>{
+           if (prc[index].innerHTML == arr[0]){
+               tip = temp * 0.05;
+               tipAmount.innerHTML = `$ ${parseFloat(tip).toFixed(2)}`;
+           }
+           else if (prc[index].innerHTML == arr[1]){
+               tip = temp * 0.10;
+               tipAmount.innerHTML = `$ ${parseFloat(tip).toFixed(2)}`;
+           }
+           else if (prc[index].innerHTML == arr[2]){
+               tip = temp * 0.15;
+               tipAmount.innerHTML = `$ ${parseFloat(tip).toFixed(2)}`;
+           }
+           else if (prc[index].innerHTML == arr[3]){
+               tip = temp * 0.25;
+               tipAmount.innerHTML = `$ ${parseFloat(tip).toFixed(2)}`;
+           }
+           else if (prc[index].innerHTML == arr[4]){
+               tip = temp * 0.50;
+               tipAmount.innerHTML = `$ ${parseFloat(tip).toFixed(2)}`;
+           }
+     
+    
+        })
+      })
+    
+    
+     
     //    console.log(`The bill is ${num1} * ${num2} = ${parseFloat(num1*num2).toFixed(2)}`);
 }
 
 bill.addEventListener('keyup', ()=>{
-    num = Number(bill.value);
+    num = bill.value;
     i = true;
     billMuliplier(num, i);
 })
 
 people.addEventListener('keyup', ()=>{
-    pep = Number(people.value);
+    pep = people.value;
     billMuliplier(pep);
 })
+
+
 
 
 
